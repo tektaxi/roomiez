8ad82b6eceb43bed852382b7a24092b17902e1be
BillSkills <billskills01@gmail.com>
2024-11-23 15:20:03 -0500

    oops public API key

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,36 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getFirestore } from "firebase/firestore";
-import { collection, addDoc } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCQz3X2FCM2REHIqMArBJFHECkxsfUj--w",
+    apiKey: process.env.API_KEY,
     authDomain: "roomiez-codejam14.firebaseapp.com",
     databaseURL: "https://roomiez-codejam14-default-rtdb.firebaseio.com",
     projectId: "roomiez-codejam14",
     storageBucket: "roomiez-codejam14.firebasestorage.app",
     messagingSenderId: "470877661579",
     appId: "1:470877661579:web:0acf78c9fa05401553a78e",
-    measurementId: "G-G0TH4FXCJY"
+    databaseURL: "https://roomiez-codejam14-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-
-try {
-    const docRef = await addDoc(collection(db, "users"), {
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815
-    });
-    console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-    console.error("Error adding document: ", e);
-}
+const database = getDatabase(app);
